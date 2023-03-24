d8f329a0356defd16b70c3d465400efdf81b7a6e
sekhar kumar dash <sekharkumardash229@gmail.com>
2023-03-24 18:46:45 +0530

    modified unittest1.ts
    Signed-off-by: sekhar kumar dash <sekharkumardash229@gmail.com>

--- newtest/unittest1.ts
+++ newtest/unittest1.ts
@@ -1,68 +1,218 @@
-import fetch, { RequestInit } from "node-fetch";
-import { wrappedNodeFetch } from "../integrations/octokit/require";
-import { createExecutionContext, getExecutionContext} from '../src/context';
+import { wrappedNodeFetch } from '../integrations/octokit/require';
+import { Response } from 'node-fetch';
+import fetch from 'node-fetch';
+import { createExecutionContext, getExecutionContext } from '../src/context';
 import { HTTP } from '../src/keploy';
 
-describe("wrappedNodeFetch", () => {
-  afterEach(() => {
-    fetchMock.restore();
+describe('wrappedNodeFetch', () => {
+  it('should return mocked response in test mode - case 1', async () => {
+    const mockResponse = new Response('mocked response');
+    const ctx = {
+      mode: 'test',
+      testId: 'testId',
+      mocks: [
+        {
+          Version: 'V1_BETA2',
+          Name: 'testId',
+          Kind: HTTP,
+          Spec: {
+            Metadata: {
+              name: 'node-fetch',
+              url: 'https://api.keploy.io/healthz',
+              options: { method: 'GET' },
+              type: 'HTTP_CLIENT',
+            },
+            Req: {
+              URL: 'https://api.keploy.io/healthz',
+              Body: '',
+              Header: {},
+              Method: 'GET',
+            },
+            Res: {
+              StatusCode: 200,
+              Header: { 'content-type': { Value: ['text/plain'] } },
+              Body: 'mocked response',
+            },
+          },
+        },
+      ],
+      deps: [],
+    
+    };
+    createExecutionContext(ctx)
+
+    const wrappedFetch = (wrappedNodeFetch(fetch) as any).bind({ fetch });
+    const url = 'https://api.keploy.io/healthz';
+    const options = {
+      method: 'GET',
+    };
+    const response = await wrappedFetch(url, options);
+    const updatedctx= getExecutionContext().context;
+    expect(response.status).toEqual(mockResponse.status);
+    expect(response.statusText).toEqual(mockResponse.statusText);
+
+    const mocks=updatedctx.mocks.length;
+    expect(mocks).toBe(0);
   });
 
-  it("should make a network request in record mode", async () => {
+  it('should return mocked response in test mode - case 2', async () => {
+    const mockResponse = new Response('mocked response');
     const ctx = {
-      mode: "record",
-      testId: "test-id",
+      mode: 'test',
+      testId: 'testId',
+      mocks: [
+        {
+          Version: 'V1_BETA2',
+          Name: 'testId',
+          Kind: HTTP,
+          Spec: {
+            Metadata: {
+              name: 'node-fetch',
+              url: 'https://api.keploy.io/status',
+              options: { method: 'GET' },
+              type: 'HTTP_CLIENT',
+            },
+            Req: {
+              URL: 'https://api.keploy.io/status',
+              Body: '',
+              Header: {},
+              Method: 'GET',
+            },
+            Res: {
+              StatusCode: 200,
+              Header: { 'content-type': { Value: ['text/plain'] } },
+              Body: 'mocked response',
+            },
+          },
+        },
+      ],
+      deps: [],
+    
+    };
+    createExecutionContext(ctx)
+
+    const wrappedFetch = (wrappedNodeFetch(fetch) as any).bind({ fetch });
+    const url = 'https://api.keploy.io/status';
+    const options = {
+      method: 'GET',
+    };
+    const response = await wrappedFetch(url, options);
+    const updatedctx= getExecutionContext().context;
+    expect(response.status).toEqual(mockResponse.status);
+    expect(response.statusText).toEqual(mockResponse.statusText);
+
+    const mocks=updatedctx.mocks.length;
+    expect(mocks).toBe(0);
+  });
+  it('should record an HTTP request in record mode', async () => {
+    const ctx = {
+      mode: 'record',
+      testId: 'testId',
+      mocks: [],
+      deps: [],     
+    };
+    createExecutionContext(ctx)
+    const wrappedFetch = (wrappedNodeFetch(fetch) as any).bind({ fetch });
+    const url = 'https://api.example.com/test';
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ data: 'test' }),
+    };
+    const response = await wrappedFetch(url, options);
+    const updatedctx = getExecutionContext().context;
+    const responseBody = await response.text();
+    const recordedOutput = updatedctx.mocks[0].Spec.Res.Body;
+    expect(response).toBeInstanceOf(Response);
+    expect(updatedctx.mocks.length).toBeGreaterThan(0);
+    expect(updatedctx.deps.length).toBeGreaterThan(0);
+    expect(response).toHaveProperty('body');
+    expect(responseBody).toEqual(recordedOutput);
+  });
+
+  it('should handle an HTTP error in record mode', async () => {
+    const ctx = {
+      mode: 'record',
+      testId: 'testId',
       mocks: [],
       deps: [],
     };
-    const url = "https://api.github.com/repos/octocat/hello-world/issues/123";
-    const options: RequestInit = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "token secret123",
-      },
+    createExecutionContext(ctx)
+    const wrappedFetch = (wrappedNodeFetch(fetch) as any).bind({ fetch });
+    const url = 'https://api.example.com/error';
+    const options = {
+      method: 'GET',
     };
-    const fetchSpy = jest.spyOn(fetch, "default").mockReturnValue(
-      Promise.resolve({
-        status: 200,
-        text: () => Promise.resolve("Hello world!"),
-        headers: {
-          "Content-Type": "text/plain",
+    await expect(wrappedFetch(url, options)).rejects.toThrow('Not Found');
+    const updatedctx = getExecutionContext().context;
+    expect(updatedctx.mocks.length).toBeGreaterThan(0);
+    expect(updatedctx.deps.length).toBeGreaterThan(0);
+  });
+  it('should call fetch function with correct arguments in off mode', async () => {
+    const mockFetch = jest.fn().mockResolvedValueOnce(new Response());
+    const ctx = {
+      context: 'off',
+      testId: 'testId',
+      mocks: [],
+      deps: [],
+    };
+    createExecutionContext(ctx)
+
+    const wrappedFetch = (wrappedNodeFetch(mockFetch) as any).bind({ fetch: mockFetch });
+    const url = 'https://api.keploy.io/healthz';
+    const options = {
+      method: 'GET',
+    };
+    const response = await wrappedFetch(url, options);
+
+    expect(mockFetch).toHaveBeenCalledWith(url, options);
+    expect(response).toBeInstanceOf(Response);
+  });
+
+  it('should return an error if fetch is called in off mode with a mock', async () => {
+    const mockFetch = jest.fn().mockResolvedValue(new Response());
+    const consoleSpy = jest.spyOn(console, 'error').mockImplementation();
+    const ctx = {
+      context: 'off',
+      testId: 'testId',
+      mocks: [
+        {
+          Version: 'V1_BETA2',
+          Name: 'testId',
+          Kind: HTTP,
+          Spec: {
+            Metadata: {
+              name: 'node-fetch',
+              url: 'https://api.keploy.io/healthz',
+              options: { method: 'GET' },
+              type: 'HTTP_CLIENT',
+            },
+            Req: {
+              URL: 'https://api.keploy.io/healthz',
+              Body: '',
+              Header: {},
+              Method: 'GET',
+            },
+            Res: {
+              StatusCode: 200,
+              Header: { 'content-type': { Value: ['text/plain'] } },
+              Body: 'mocked response',
+            },
+          },
         },
-      })
-    );
-
-    const wrappedFetch = wrappedNodeFetch(fetch);
-
-    await wrappedFetch.call({ fetch }, url, options);
-
-    expect(fetchSpy).toHaveBeenCalledWith(url, {
-      method: options.method,
-      headers: options.headers,
-    });
-
-    expect(ctx.mocks).toHaveLength(1);
-
-    const httpMock = ctx.mocks[0] as HTTP;
-    expect(httpMock.Kind).toEqual("HTTP");
-    expect(httpMock.Spec.Metadata.name).toEqual("node-fetch");
-    expect(httpMock.Spec.Metadata.url).toEqual(url);
-    expect(httpMock.Spec.Metadata.type).toEqual("HTTP_CLIENT");
-    expect(httpMock.Spec.Metadata.options).toEqual(options);
-
-    expect(httpMock.Spec.Req.Method).toEqual(options.method);
-    expect(httpMock.Spec.Req.URL).toEqual(url);
-    expect(httpMock.Spec.Req.Header).toEqual({
-      "Content-Type": "application/json",
-      Authorization: "token secret123",
-    });
-    expect(httpMock.Spec.Req.Body).toBeUndefined();
-
-    expect(httpMock.Spec.Res.StatusCode).toEqual(200);
-    expect(httpMock.Spec.Res.Header).toEqual({
-      "Content-Type": "text/plain",
-    });
-    expect(await httpMock.Spec.Res.Body?.asString()).toEqual("Hello world!");
+      ],
+      deps: [],
+    };
+    createExecutionContext(ctx)
+    const wrappedFetch = (wrappedNodeFetch(mockFetch) as any).bind({ fetch: mockFetch });
+    const url = 'https://api.keploy.io/healthz';
+    const options = {
+      method: 'GET',
+    };
+    const response = await wrappedFetch(url, options);
+    expect(consoleSpy).toHaveBeenCalledWith('Cannot mock dependencies in off mode');
+    expect(response).toBeUndefined();
   });
 });
