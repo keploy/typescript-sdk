7a27025d37c673be20f90aa279e95045e58b4363
Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>
2024-05-13 15:38:53 +0530

    chore: remove merge descriptor
    Signed-off-by: Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>

--- v2/dedup/register.ts
+++ v2/dedup/register.ts
@@ -3,7 +3,7 @@ import Hook from "require-in-the-middle";
 import expressMiddleware from "./middleware";
 import bodyParser from "body-parser";
 import cors from "cors";
-import mixin from "merge-descriptors";
+// import mixin from "merge-descriptors";
 
 
 // @ts-ignore
@@ -21,7 +21,7 @@ Hook(["express"], function (exports) {
 
   // copy the properties and methods of exported Function object into wrapped Funtion(keployWrappedExpress).
   // In order to prevent "express._Method_ or express._Field_ is not declared" error.
-  mixin(keployWrappedExpress, expressApp, false);
+  // mixin(keployWrappedExpress, expressApp, false);
   exports = keployWrappedExpress;
   return exports;
 });
