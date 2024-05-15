8246f85f52e5bb0a67ba969ae6e807c210f8e97e
Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>
2024-05-15 11:42:16 +0530

    chore: add khook in register
    Signed-off-by: Sarthak Shyngle <50234097+Sarthak160@users.noreply.github.com>

--- v2/dedup/register.ts
+++ v2/dedup/register.ts
@@ -7,24 +7,24 @@ import cors from "cors";
 const _ = require('lodash');
 const khook = "keployWrappedExpress";
 // @ts-ignore
-// Hook(["express"], function (exports) {
-//   console.log("Inside keploy hook...");
-//   const expressApp = exports;
-//   function keployWrappedExpress() {
-//     const keployApp = expressApp();
+Hook(["express"], function (exports) {
+  console.log("Inside keploy hook...");
+  const expressApp = exports;
+  function keployWrappedExpress() {
+    const keployApp = expressApp();
 
-//     keployApp.use(bodyParser.json());
-//     keployApp.use(cors());
-//     keployApp.use(expressMiddleware());
-//     keployApp.appliedMiddleware = true;
-//     return keployApp;
-//   }
+    keployApp.use(bodyParser.json());
+    keployApp.use(cors());
+    keployApp.use(expressMiddleware());
+    keployApp.appliedMiddleware = true;
+    return keployApp;
+  }
   
-//   // copy the properties and methods of exported Function object into wrapped Funtion(keployWrappedExpress).
-//   // In order to prevent "express._Method_ or express._Field_ is not declared" error.
-//   // mixin(keployWrappedExpress, expressApp, false);
-//   _.assign(keployWrappedExpress, expressApp);
-//   exports = keployWrappedExpress;
-//   return exports;
-// });
-export { expressMiddleware };
+  // copy the properties and methods of exported Function object into wrapped Funtion(keployWrappedExpress).
+  // In order to prevent "express._Method_ or express._Field_ is not declared" error.
+  // mixin(keployWrappedExpress, expressApp, false);
+  _.assign(keployWrappedExpress, expressApp);
+  exports = keployWrappedExpress;
+  return exports;
+});
+export { khook };
